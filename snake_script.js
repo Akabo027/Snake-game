a1740ea06c05ed32a03c8183e7f7dab10900846f
Arthur Kaboré <unkabore@gmail.com>
2022-08-09 10:05:53 -0400

    refresh canvas

--- snake_script.js
+++ snake_script.js
@@ -1,13 +1,32 @@
 //"window.onload" ---meaning--> when the page loads
 window.onload = function() {
-  var canvas = document.createElement("canvas")
-  canvas.width = 900;
-  canvas.height = 600;
-  canvas.style.border = "1px solid";
-  document.body.appendChild(canvas); // attaches a tag to this body
 
-  var ctx = canvas.getContext('2d'); // specifies the dimension we're drawing in
-  ctx.fillStyle = "#919611"; // gives a color to the drawing
-  ctx.fillRect(30, 30, 100, 50); // (x, y, longueur, largeur), the left top-corner as origin
+  var canvas;
+  var ctx;
+  var delay = 100;
+  var xCoord = 0;
+  var yCoord = 0;
 
+  init();
+
+  function init() {
+    canvas = document.createElement("canvas");
+    canvas.width = 900;
+    canvas.height = 600;
+    canvas.style.border = "1px solid";
+    document.body.appendChild(canvas); // attaches a tag to this body
+
+    ctx = canvas.getContext('2d'); // specifies the dimension we're drawing in
+    refreshCanvas();
+  }
+
+  function refreshCanvas() {
+    xCoord += 2;
+    yCoord += 2;
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // resets(deletes) the previous rectangle
+    ctx.fillStyle = "#919611"; // gives a color to the drawing
+    ctx.fillRect(xCoord, yCoord, 100, 50); // (x, y, longueur, largeur), the left top-corner as origin
+    setTimeout(refreshCanvas, delay); // executes a function (first parameter) after a certain time (second parameter)
+
+  }
 }
