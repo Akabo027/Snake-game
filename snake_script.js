63ddcbbaf41a56cb273d38789f4f949916ac5052
Arthur Kaboré <unkabore@gmail.com>
2022-08-18 10:34:28 -0400

    snake direction

--- snake_script.js
+++ snake_script.js
@@ -7,6 +7,7 @@ window.onload = function() {
   var ctx;
   var delay = 100;
   var snakyy;
+  var previous;
 
   init();
 
@@ -18,7 +19,8 @@ window.onload = function() {
     document.body.appendChild(canvas); // attaches a tag to this body
 
     ctx = canvas.getContext('2d'); // specifies the dimension we're drawing in
-    snakyy = new Snake([[5,4],[4,4],[3,4]]);
+    snakyy = new Snake([[2,0],[1,0],[0,0]]);
+    previous = snakyy.direction;
     refreshCanvas();
   }
 
@@ -39,7 +41,7 @@ window.onload = function() {
 
   function Snake(body) {
     this.body = body;
-
+    this.direction = "ArrowRight";
     this.draw = function() {
       ctx.save();
       ctx.fillStyle = "#919611";
@@ -52,11 +54,53 @@ window.onload = function() {
 
     this.advance = function(){
       var nextPosition = this.body[0].slice(); // slice() makes a copy of the element
-      nextPosition[0]++;
+
+      const bod = document.querySelector("#b");
+      bod.addEventListener('keydown', (event) => this.direction = event.key);
+      //console.log(this.direction);
+      switch (this.direction) {
+        case "ArrowLeft":
+          // if already in the horizontale position, it continues in the same direction
+          if (previous != "ArrowRight") {
+            nextPosition[0]--;
+          }
+          break;
+        case "ArrowRight":
+          // if already in the horizontale position, it continues in the same direction
+          if (previous != "ArrowLeft") {
+            nextPosition[0]++;
+          }
+          break;
+        case "ArrowUp":
+          // if already in the vertical position, it continues in the same direction
+          if (previous != "ArrowDown") {
+            nextPosition[1]--;
+          }
+          break;
+        case "ArrowDown":
+          // if already in the vertical position, it continues in the same direction
+          if (previous != "ArrowUp") {
+            nextPosition[1]++;
+          }
+          break;
+      };
+
+      previous = this.direction;
+
       if ((nextPosition[0] * blockSize) >= canvasWidth)  {
         nextPosition[0] = 0;
       }
-      this.body.unshift(nextPosition);
+      else if ((nextPosition[1] * blockSize) >= canvasHeight) {
+        nextPosition[1] = 0;
+      }
+      else if (this.direction == "ArrowLeft" && nextPosition[0] == 0) {
+        nextPosition[0] = canvasWidth;
+      }
+      else if (this.direction == "ArrowUp" && nextPosition[1] == 0){
+        nextPosition[1] = canvasHeight;
+      }
+
+      this.body.unshift(nextPosition); // add the new position at the first position of the body (the array)
       this.body.pop();
       //nextPosition[1]++;
     };
